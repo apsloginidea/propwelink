3cb668bcfad75a6b55db207cb05c7f87b1749b26
apsloginidea <aps.login.idea@gmail.com>
2026-06-17 19:25:56 +0800

    Update label-engine.js

--- label-engine.js
+++ label-engine.js
@@ -7,8 +7,8 @@ const labelConfig = {
   "footer_note": "© 2026 Propwelink Pvt Ltd",
   "submit_contact_details":"ttps://formspree.io/f/xrevjbjn",
   "website_msg1": "We are Real Estate Advisory Company.",
-  "website_msg2": "Our website is under deveplopment phase,",
-  "website_msg3": "meanwhile if you are looking for investment in real estate market,",
+  "website_msg2": "Our website is under deveplopment phase.",
+  "website_msg3": "Meanwhile if you are looking for investment in real estate market,",
   "website_msg4": "then leave us your contact deails, our team will call you back soon"
 };
 
